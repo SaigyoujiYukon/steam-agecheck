4f2d5b619f6d29b2e2e0c00506c2781783c037cd
SaigyoujiYukon <40208202+SaigyoujiYukon@users.noreply.github.com>
2020-08-17 02:07:32 +0800

    Update ababab.user.js
    。

--- ababab.user.js
+++ ababab.user.js
@@ -2,7 +2,7 @@
 // @name           steam重定向
 // @version        0.1
 // @author         SaigyoujiYukon
-// @description    Ð¼steam±ðÏëÄêÁäÑéÖ¤
+// @description    屑steam别想区域不可用
 
 // @include        https://store.steampowered.com/agecheck/app/*
 
